1edca1ef6f1478348cb0386c5c8847bf769b5e1b
Rodwell Leo <leorodwel86@gmail.com>
2024-09-07 23:12:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const fetchAllProducts_1 = require("./functions/fetchAllProducts");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: "*"
+}));
+app.get("/", (req, res) => {
+    res.send("Server is getting requests");
+});
+app.get("/proxy/products", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield (0, fetchAllProducts_1.fetchAllProducts)();
+        res.send(response);
+    }
+    catch (e) {
+        res.send([]);
+    }
+}));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`[server]: server is listening in port ${PORT}`);
+});
---
+++ app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv"
+import { fetchAllProducts } from "./functions/fetchAllProducts";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: "*"
+}))
+
+app.get("/", (req, res) => {
+
+    res.send("Server is getting requests");
+})
+
+app.get("/proxy/products", async (req, res) => {
+    try{
+        const response = await fetchAllProducts();
+        res.send(response);
+    }catch(e){
+        res.send([])
+    }
+    
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`[server]: server is listening in port ${PORT}`);
+})
